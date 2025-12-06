c212d877dc12c501fa08bf5fb5444bebd214b047
HJAguilera1981 <hagui018@jaguar.tamu.edu>
2025-12-06 20:11:39 +0000

    Semester Project changes

--- SemesterProject_Herman_Aguilera.java
+++ SemesterProject_Herman_Aguilera.java
@@ -1,21 +1,20 @@
+
 /**
  * SemesterProject_Herman_Aguilera
  *
- * For semester project I will determine how many miles I travel to and from work,
- * daily, weekly, monthly and yearly
- * Will also determine amount of gas used by establishing a miles per gallon rate of 16
- * And finally cost of gas with an averge price of $2.50 per gallon
- * 
+ * For my semester project I will determine how many miles I travel to and from work
+ * daily, weekly, monthly, and yearly.
+ * I will also determine the amount of gas used by establishing a miles-per-gallon
+ * rate of 16 MPG.
+ * Finally, I will calculate the cost of gas using an average price of $2.50 per gallon.
+ *
  * Herman Aguilera
  * December 6, 2025
+ */
 
+public class CommuteCalculator {
 
-public class CommuteCalculator 
-
-{
-
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         double oneWay = 23;
         double mpg = 16;
         double gasPrice = 2.50;
@@ -26,14 +25,14 @@ public class CommuteCalculator
         calculate("Yearly", 260, oneWay, mpg, gasPrice);
     }
 
-    public static void calculate(String label, int days, double oneWay, double mpg, double gasPrice) 
-    {
+    public static void calculate(String label, int days, double oneWay, double mpg, double gasPrice) {
         double miles = oneWay * 2 * days;
         double gallons = miles / mpg;
         double cost = gallons * gasPrice;
 
-        System.out.printf("%s: %.0f miles, %.2f gallons, $%.2f%n", 
-                            label, miles, gallons, cost);
+        System.out.printf("%s: %.0f miles, %.2f gallons, $%.2f%n",
+                label, miles, gallons, cost);
     }
 }
+
 
